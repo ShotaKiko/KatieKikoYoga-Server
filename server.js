b6fa82561ec0a541e566be7e18ff7a0143ecd58c
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-10-14 17:11:31 -0500

    imported dependencies for use

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const server = express();
+const cors = require("cors");
+const logger = require("morgan");
+const helmet = require("helmet");
+
+//~~~~~~~~~~Routers~~~~~~~~~~~~~~~~~~~~
+// const userRouter = require('./routes/userRouter.js')
+
 
 server.use(express.json());
+server.use(helmet());
+server.use(
+  cors({
+    origin: "*",
+  })
+);
+server.use(logger("dev"));
+
+// server.use('/users', userRouter) TODO -- implemnent userRouter
 
 server.get("/", (req, res) => {
   res.status(200).json({ api: "up", timestamp: Date.now() });
